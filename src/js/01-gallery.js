47e25d3fd35e39b6933aa89ef563180496d45925
GG_Ray <110432497+alecente@users.noreply.github.com>
2023-05-04 22:32:52 -0500

    gallery 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
-import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
